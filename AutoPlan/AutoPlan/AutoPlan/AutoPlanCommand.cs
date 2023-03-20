775f6a28cf83069b1505f68a2575be40293d8f64
csgen <csgen@umich.edu>
2023-03-20 22:22:07 +0800

    Update

--- AutoPlan/AutoPlan/AutoPlan/AutoPlanCommand.cs
+++ AutoPlan/AutoPlan/AutoPlan/AutoPlanCommand.cs
@@ -20,68 +20,14 @@ namespace AutoPlan.AutoPlan
             // plug-in, so it is safe to store a refence in a static property.
             Instance = this;
         }
-
         ///<summary>The only instance of this command.</summary>
         public static AutoPlanCommand Instance { get; private set; }
-
         ///<returns>The command name as it appears on the Rhino command line.</returns>
         public override string EnglishName => "AutoPlanCommand";
-
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
             // TODO: start here modifying the behaviour of your command.
             // ---
-            #region
-            //RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
-
-            //Point3d pt0;
-            //using (GetPoint getPointAction = new GetPoint())
-            //{
-            //    getPointAction.SetCommandPrompt("Please select the start point");
-            //    if (getPointAction.Get() != GetResult.Point)
-            //    {
-            //        RhinoApp.WriteLine("No start point was selected.");
-            //        return getPointAction.CommandResult();
-            //    }
-            //    pt0 = getPointAction.Point();
-            //}
-
-            //Point3d pt1;
-            //using (GetPoint getPointAction = new GetPoint())
-            //{
-            //    getPointAction.SetCommandPrompt("Please select the E point");
-            //    getPointAction.SetBasePoint(pt0, true);
-            //    getPointAction.DynamicDraw +=
-            //      (sender, e) => e.Display.DrawLine(pt0, e.CurrentPoint, System.Drawing.Color.DarkRed);
-            //    if (getPointAction.Get() != GetResult.Point)
-            //    {
-            //        RhinoApp.WriteLine("No end point was selected.");
-            //        return getPointAction.CommandResult();
-            //    }
-            //    pt1 = getPointAction.Point();
-            //}
-
-            //doc.Objects.AddLine(pt0, pt1);
-            //doc.Views.Redraw();
-            //RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
-            #endregion
-
-            //Point3d pt0;
-            //Curve curve1;
-            //List<Curve> localcurve = new List<Curve>();
-
-
-            //if (paths.Count > 0)
-            //{
-            //    PlaneObjectManager planeObjectM = new PlaneObjectManager();
-            //    planeObjectM.Buildings = buildings;
-            //    planeObjectM.Paths = paths;
-            //    P2P_Path p1 = new P2P_Path(inputPoints, planeObjectM);
-
-            //    doc.Objects.AddCurve(p1.MidCurve);
-            //}
-
-            //RhinoApp.WriteLine(pathObject.PathBreps.Length.ToString());
             //foreach(Curve curve in localcurve)
             //{
             //    foreach (Brep pipe in Brep.CreatePipe(curve, 5, true, PipeCapMode.None, true, 0.01, 0.01))
@@ -91,9 +37,6 @@ namespace AutoPlan.AutoPlan
             //}
             Commands.DrawP2P_Path(doc);
             doc.Views.Redraw();
-
-            //RhinoApp.WriteLine("The");
-
             // ---
             return Result.Success;
         }
